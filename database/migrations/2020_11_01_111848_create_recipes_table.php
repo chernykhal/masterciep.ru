5a4587f79a194fcb5115a9aad05566530f5ed1d1
Alexander Chernykh <al.ed.chernykh@gmail.com>
2020-11-01 18:04:49 +0500

    init

---
+++ database/migrations/2020_11_01_111848_create_recipes_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image_url');
+            $table->string('video_url')->nullable();
+            $table->longText('process');
+            $table->integer('raiting')->nullable()->default(5);
+            $table->timestamps();
+        });
+        Schema::create('recipes_products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('recipe_id')->constrained('recipes');
+            $table->foreignId('product_id')->constrained('products');
+            $table->integer('unit_value');
+            $table->timestamps();
+        });
+        Schema::create('users_recipes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('recipe_id')->constrained('recipes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+        Schema::dropIfExists('recipes_products');
+        Schema::dropIfExists('users_recipes');
+    }
+}
