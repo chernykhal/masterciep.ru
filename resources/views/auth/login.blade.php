bfbdfd9c58517226b529f0428a0345a5a09e47d6
Alexander Chernykh <al.ed.chernykh@gmail.com>
2020-11-03 17:37:07 +0500

    (feat) edit login and registration forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,34 +14,29 @@
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
-
             <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" placeholder="Электронная почта" :value="old('email')" required autofocus />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" placeholder="Пароль" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <input id="remember_me" type="checkbox" class="form-checkbox" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Запомнить меня') }}</span>
                 </label>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Login') }}
+            <div class="flex items-center justify-end mt-4 w-full">
+                <x-jet-button class="justify-center w-full btn-primary">
+                    {{ __('Войти') }}
                 </x-jet-button>
+            </div>
+            <div class="flex items-center justify-end mt-4 w-full">
+                <a class="inline-flex items-center px-4 py-2 text-center border border-transparent rounded-full font-semibold text-xs tracking-widest disabled:opacity-25 transition ease-in-out duration-150 justify-center w-full btn-secondary" href="{{ route('register') }}">
+                    {{ __('Регистрация') }}
+                </a>
             </div>
         </form>
     </x-jet-authentication-card>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,37 +1,44 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <x-jet-authentication-card-logo/>
         </x-slot>
 
-        <x-jet-validation-errors class="mb-4" />
+        <x-jet-validation-errors class="mb-4"/>
 
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
             <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-jet-input id="email" class="block mt-1 w-full" placeholder="Электронная почта" type="email" name="email" :value="old('email')"
+                             required/>
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                <x-jet-input id="password" class="block mt-1 w-full" placeholder="Пароль" type="password" name="password" required
+                             autocomplete="new-password"/>
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                <x-jet-input id="password_confirmation" class="block mt-1 w-full" placeholder="Подтвердите пароль" type="password"
+                             name="password_confirmation" required autocomplete="new-password"/>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-jet-button class="ml-4">
-                    {{ __('Register') }}
+            <div class="block mt-4">
+                <label for="confirmation" class="flex items-center">
+                    <input id="confirmation" type="checkbox" class="form-checkbox" name="confirmation">
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Я согласен с обработкой персональных данных') }}</span>
+                </label>
+            </div>
+            <div class="flex items-center justify-end mt-4 w-full">
+                <x-jet-button class="justify-center w-full btn-primary">
+                    {{ __('Зарегистрироваться') }}
                 </x-jet-button>
+            </div>
+            <div class="flex items-center justify-end mt-4 w-full">
+                <a class="inline-flex items-center px-4 py-2 text-center border border-transparent rounded-full font-semibold text-xs tracking-widest disabled:opacity-25 transition ease-in-out duration-150 justify-center w-full btn-secondary"
+                   href="{{ route('login') }}">
+                    {{ __('Вход') }}
+                </a>
             </div>
         </form>
     </x-jet-authentication-card>
