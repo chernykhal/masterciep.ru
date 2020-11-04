b926ff440ece3a7ebe460721aa62a8fd55ff4979
Alexander Chernykh <al.ed.chernykh@gmail.com>
2020-11-04 15:19:09 +0500

    (feat) edit profile page

--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -2,7 +2,7 @@
     <app-layout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Profile
+                Настройки профиля
             </h2>
         </template>
 
@@ -14,19 +14,19 @@
 
                 <update-password-form class="mt-10 sm:mt-0" />
 
-                <div v-if="$page.jetstream.canManageTwoFactorAuthentication">
-                    <jet-section-border />
-
-                    <two-factor-authentication-form class="mt-10 sm:mt-0" />
-                </div>
-
-                <jet-section-border />
-
-                <logout-other-browser-sessions-form :sessions="sessions" class="mt-10 sm:mt-0" />
-
-                <jet-section-border />
-
-                <delete-user-form class="mt-10 sm:mt-0" />
+<!--                <div v-if="$page.jetstream.canManageTwoFactorAuthentication">-->
+<!--                    <jet-section-border />-->
+<!---->
+<!--                    <two-factor-authentication-form class="mt-10 sm:mt-0" />-->
+<!--                </div>-->
+<!---->
+<!--                <jet-section-border />-->
+<!---->
+<!--                <logout-other-browser-sessions-form :sessions="sessions" class="mt-10 sm:mt-0" />-->
+<!---->
+<!--                <jet-section-border />-->
+<!---->
+<!--                <delete-user-form class="mt-10 sm:mt-0" />-->
             </div>
         </div>
     </app-layout>
--- resources/js/Pages/Profile/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/UpdateProfileInformationForm.vue
@@ -1,49 +1,17 @@
 <template>
     <jet-form-section @submitted="updateProfileInformation">
         <template #title>
-            Profile Information
+            Данные профиля
         </template>
 
         <template #description>
-            Update your account's profile information and email address.
+            Измените данные своего профиля
         </template>
 
         <template #form>
-            <!-- Profile Photo -->
-            <div class="col-span-6 sm:col-span-4" v-if="$page.jetstream.managesProfilePhotos">
-                <!-- Profile Photo File Input -->
-                <input type="file" class="hidden"
-                            ref="photo"
-                            @change="updatePhotoPreview">
-
-                <jet-label for="photo" value="Photo" />
-
-                <!-- Current Profile Photo -->
-                <div class="mt-2" v-show="! photoPreview">
-                    <img :src="user.profile_photo_url" alt="Current Profile Photo" class="rounded-full h-20 w-20 object-cover">
-                </div>
-
-                <!-- New Profile Photo Preview -->
-                <div class="mt-2" v-show="photoPreview">
-                    <span class="block rounded-full w-20 h-20"
-                          :style="'background-size: cover; background-repeat: no-repeat; background-position: center center; background-image: url(\'' + photoPreview + '\');'">
-                    </span>
-                </div>
-
-                <jet-secondary-button class="mt-2 mr-2" type="button" @click.native.prevent="selectNewPhoto">
-                    Select A New Photo
-                </jet-secondary-button>
-
-                <jet-secondary-button type="button" class="mt-2" @click.native.prevent="deletePhoto" v-if="user.profile_photo_path">
-                    Remove Photo
-                </jet-secondary-button>
-
-                <jet-input-error :message="form.error('photo')" class="mt-2" />
-            </div>
-
             <!-- Email -->
             <div class="col-span-6 sm:col-span-4">
-                <jet-label for="email" value="Email" />
+                <jet-label for="email" value="Электронная почта" />
                 <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" />
                 <jet-input-error :message="form.error('email')" class="mt-2" />
             </div>
@@ -51,11 +19,11 @@
 
         <template #actions>
             <jet-action-message :on="form.recentlySuccessful" class="mr-3">
-                Saved.
+                Сохранено.
             </jet-action-message>
 
             <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Save
+                Сохранить
             </jet-button>
         </template>
     </jet-form-section>
