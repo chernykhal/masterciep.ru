76cc33ab57d10de5dfb2e1d5538884483909385e
Alexander Chernykh <al.ed.chernykh@gmail.com>
2020-11-09 21:30:21 +0500

    (feat) edit navbar

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -23,6 +23,68 @@
 <!--                            <jet-nav-link :href="route('recipes')" :active="route().current('recipes')">-->
 <!--                                Рецепты-->
 <!--                            </jet-nav-link>-->
+                            <div class="hidden sm:flex sm:items-center sm:ml-6">
+                                <div class="ml-3 relative">
+                                    <jet-dropdown align="right" width="48">
+                                        <template #trigger>
+                                            <button v-if="$page.jetstream.managesProfilePhotos" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                                                <img class="h-8 w-8 rounded-full object-cover" :src="$page.user.profile_photo_url" :alt="$page.user.name" />
+                                            </button>
+
+                                            <button v-else class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                                <div>Продукты</div>
+
+                                                <div class="ml-1">
+                                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                                    </svg>
+                                                </div>
+                                            </button>
+                                        </template>
+
+                                        <template #content>
+                                            <jet-dropdown-link :href="route('my.products')">
+                                                Мой холодильник
+                                            </jet-dropdown-link>
+                                            <div class="border-t border-gray-100"></div>
+                                            <jet-dropdown-link :href="route('types.index')">
+                                                Добавить продукты
+                                            </jet-dropdown-link>
+                                        </template>
+                                    </jet-dropdown>
+                                </div>
+                            </div>
+                            <div class="hidden sm:flex sm:items-center sm:ml-6">
+                                <div class="ml-3 relative">
+                                    <jet-dropdown align="right" width="48">
+                                        <template #trigger>
+                                            <button v-if="$page.jetstream.managesProfilePhotos" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                                                <img class="h-8 w-8 rounded-full object-cover" :src="$page.user.profile_photo_url" :alt="$page.user.name" />
+                                            </button>
+
+                                            <button v-else class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                                <div>Рецепты</div>
+
+                                                <div class="ml-1">
+                                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                                    </svg>
+                                                </div>
+                                            </button>
+                                        </template>
+
+                                        <template #content>
+                                            <jet-dropdown-link :href="route('my.recipes')">
+                                                Доступные рецепты
+                                            </jet-dropdown-link>
+                                            <div class="border-t border-gray-100"></div>
+                                            <jet-dropdown-link :href="route('recipes.index')">
+                                               Все рецепты
+                                            </jet-dropdown-link>
+                                        </template>
+                                    </jet-dropdown>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
@@ -103,6 +165,18 @@
                     <div class="mt-3 space-y-1">
                         <jet-responsive-nav-link :href="route('profile.show')" :active="route().current('profile.show')">
                             Настройки
+                        </jet-responsive-nav-link>
+                        <jet-responsive-nav-link :href="route('my.products')" :active="route().current('my.products')">
+                            Мои продукты
+                        </jet-responsive-nav-link>
+                        <jet-responsive-nav-link :href="route('types.index')" :active="route().current('products.index')">
+                            Добавить продукты
+                        </jet-responsive-nav-link>
+                        <jet-responsive-nav-link :href="route('my.recipes')" :active="route().current('my.recipes')">
+                            Доступные рецепты
+                        </jet-responsive-nav-link>
+                        <jet-responsive-nav-link :href="route('recipes.index')" :active="route().current('recipes.index')">
+                            Все рецепты
                         </jet-responsive-nav-link>
                         <form method="POST" @submit.prevent="logout">
                             <jet-responsive-nav-link as="button">
