2b8f4ca25e3a1ae27028675a3a1f5f06020597cb
Alexander Chernykh <al.ed.chernykh@gmail.com>
2020-11-10 01:03:18 +0500

    (feat) add middleware for routes

--- routes/web.php
+++ routes/web.php
@@ -16,24 +16,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::put('products/store', [ProductController::class, 'store'])->name('products.store');
-Route::get('my/products', [ProductController::class, 'usersProductsIndex'])->name('my.products');
-Route::post('my/products/{product}', [ProductController::class, 'usersProductsUpdate'])->name('my.products.update');
-Route::post('my/products/{product}/destroy', [ProductController::class, 'usersProductsDestroy'])->name('my.products.destroy');
-Route::post('products/{product}/add', [ProductController::class, 'add'])->name('products.add');
-Route::resource('products', ProductController::class)->except('store');
+Route::put('products/store', [ProductController::class, 'store'])->name('products.store')->middleware('auth');
+Route::get('my/products', [ProductController::class, 'usersProductsIndex'])->name('my.products')->middleware('auth');
+Route::post('my/products/{product}', [ProductController::class, 'usersProductsUpdate'])->name('my.products.update')->middleware('auth');
+Route::post('my/products/{product}/destroy', [ProductController::class, 'usersProductsDestroy'])->name('my.products.destroy')->middleware('auth');
+Route::post('products/{product}/add', [ProductController::class, 'add'])->name('products.add')->middleware('auth');
+Route::resource('products', ProductController::class)->except('store')->middleware('auth');
 
-Route::put('recipes/store', [RecipeController::class, 'store'])->name('recipes.store');
-Route::get('my/recipes/', [RecipeController::class, 'usersRecipesIndex'])->name('my.recipes');
-Route::get('my/recipes/{recipe}/', [RecipeController::class, 'cook'])->name('my.recipes.cook');
-Route::resource('recipes', RecipeController::class)->except('store');
+Route::put('recipes/store', [RecipeController::class, 'store'])->name('recipes.store')->middleware('auth');
+Route::get('my/recipes/', [RecipeController::class, 'usersRecipesIndex'])->name('my.recipes')->middleware('auth');
+Route::get('my/recipes/{recipe}/', [RecipeController::class, 'cook'])->name('my.recipes.cook')->middleware('auth');
+Route::resource('recipes', RecipeController::class)->except('store')->middleware('auth');
 
-Route::put('types/store', [ProductTypeController::class, 'store'])->name('types.store');
-Route::resource('types', ProductTypeController::class)->except(['store']);
+Route::put('types/store', [ProductTypeController::class, 'store'])->name('types.store')->middleware('auth');
+Route::resource('types', ProductTypeController::class)->except(['store'])->middleware('auth');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
     return Inertia\Inertia::render('Dashboard');
 })->name('dashboard');
